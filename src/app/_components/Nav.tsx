6e10a02bd7f7518340473402a4ebb81c05b3908f
alsendrha <alsendrha@naver.com>
2024-05-08 14:40:14 +0900

    conf:develop 브랜치 생성

--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <div className="w-full h-[130px] flex justify-between max-w-[1200px] items-center my-0 mx-auto">
       <Link href='/'>메인</Link>
-      <Link href='/productList'>경매리스트</Link>
+      <Link href='/productList'>경매 리스트</Link>
       <Link href='/productList/id'>경매 디테일 페이지</Link>
     </div>
   )
