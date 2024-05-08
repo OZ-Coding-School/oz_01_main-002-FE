c2a4fbb9db94b37a1ab8b3f227f1f05862ede567
alsendrha <alsendrha@naver.com>
2024-05-08 14:39:22 +0900

    conf:초기 셋팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from 'next/font/local';
+import Nav from "./_components/Nav";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: '--font-pretendard',
+})
+
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={pretendard.className}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
