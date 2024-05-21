eb315aa3351427bde9c1a87923c10ad3778d5cfa
hyerin1228 <hyerin1228@gmail.com>
2024-05-21 17:08:53 +0900

    style: 결제완료 페이지의 초기 레이아웃 및 스타일링 수정. 열로 나열되도록 배치 구성을 바꾸고 결제페이지와 결제완료 페이지 스타일링 통합

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 import { useProductStore } from "@/_store";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 function PaymentPage() {
   const { paymentUserProducts } = useProductStore();
+  const router = useRouter();
+
+  const handleMoveConfirmation = () => {
+    router.push("/payment/confirmation");
+  };
 
   const userInfo = {
     name: "료니",
@@ -24,18 +30,22 @@ function PaymentPage() {
   const totalAmount = totalPrice + totalShipping; // 총 결제 금액
 
   return (
-    <div className="container mx-auto p-8">
-      <div className="text-3xl font-semibold my-2">
-        <p>주문/결제</p>
-      </div>
-      <div className="border-2 border-[#D1B383] mb-5" />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        {/* 왼쪽 영역 상품 정보, 구매자 정보, 결제 정보 */}
-        <div className="flex flex-col gap-3">
-          <div className="border p-4 rounded-lg shadow-sm">
+    <div className="min-h-screen bg-gray-100">
+      <div className="container mx-auto p-8 max-w-screen-lg px-4 md:px-8">
+        <div className="text-3xl font-semibold my-2">
+          <p>주문 / 결제</p>
+        </div>
+        <div className="border-2 border-[#D1B383] mb-5" />
+
+        <div className="flex flex-col gap-8">
+          {/* 주문 상품 정보 */}
+          <div className="border p-4 rounded-lg shadow-sm bg-white">
             <h2 className="text-xl font-bold mb-4">주문 상품 정보</h2>
             {paymentUserProducts.map((item) => (
-              <div key={item.id} className="flex items-center mt-6 mb-4">
+              <div
+                key={item.id}
+                className="flex items-center mt-6 mb-4 pl-4 pr-4"
+              >
                 <div className="w-[130px] h-[130px] bg-[gray] object-cover rounded-lg relative overflow-hidden">
                   <Image
                     src={item.img}
@@ -68,41 +78,43 @@ function PaymentPage() {
               </div>
             ))}
           </div>
-          <div>
-            <div className="border p-4 rounded-lg shadow-sm">
-              <h2 className="text-xl font-bold mb-4">받는 사람 정보</h2>
-              <div className="flex justify-between items-center mb-2">
-                <div className="flex flex-col gap-1">
-                  <p className="font-bold text-lg">
-                    {userInfo.name}
-                    <span className="ml-2 px-2 py-1 text-gray-600 rounded-md text-sm border">
-                      기본 배송지
-                    </span>
-                  </p>
-                  <p className="text-gray-600">{userInfo.phone} </p>
-                  <p className="text-gray-600">{userInfo.address}</p>
-                </div>
-                <button className="border rounded px-2 py-1">변경</button>
+          {/* 받는 사람 정보 */}
+          <div className="border p-4 rounded-lg shadow-sm bg-white">
+            <h2 className="text-xl font-bold mb-4">받는 사람 정보</h2>
+            <div className="flex justify-between gap-3 items-center mb-2 pl-4 pr-4">
+              <div className=" flex flex-col gap-1">
+                <p className="font-bold text-lg">
+                  {userInfo.name}
+                  <span className="ml-2 px-2 py-1 text-gray-600 rounded-md text-sm border">
+                    기본 배송지
+                  </span>
+                </p>
+                <p className="text-gray-600">{userInfo.phone} </p>
+                <p className="text-gray-600">{userInfo.address}</p>
               </div>
+              <button className="border rounded px-2 py-1">변경</button>
+            </div>
+            <div className="flex justify-between items-center mb-2  pl-4 pr-4">
               <input
                 type="text"
-                className="w-full border p-2 rounded mt-2"
+                className="w-full border rounded p-2 text-gray-800 placeholder-gray-400"
                 placeholder="메모를 입력해 주세요."
               />
             </div>
           </div>
-
-          <div className="border p-4 rounded-md shadow-sm">
+          {/* 결제 정보 */}
+          <div className="border p-4 rounded-md shadow-sm bg-white">
             <h2 className="text-xl font-bold mb-4">결제 정보</h2>
             <div className="flex justify-between items-center mb-2">
-              <p className="font-bold text-lg flex items-center">내 코인</p>
-              <p className="text-gray-600">보유: {coins}원</p>
+              <p className="font-bold text-lg flex items-center pl-4 pr-4">
+                내 코인
+              </p>
+              <p className="text-gray-600 pr-4">보유: {coins}원</p>
             </div>
-            <div className="flex justify-between items-center mb-2">
+            <div className="flex justify-between items-center mb-2  pl-4 pr-4">
               <input
                 type="text"
                 className="w-2/3 border p-2 rounded text-black-600"
-                // placeholder={`${coins}원`}
                 placeholder="사용할 코인을 입력해 주세요."
               />
               <button className="ml-2 border p-2 rounded text-black-500">
@@ -110,37 +122,37 @@ function PaymentPage() {
               </button>
             </div>
           </div>
-        </div>
-        {/* 오른쪽 영역 최종 결제 금액 정보 */}
-        <div className="relative">
-          <div className="sticky top-8 p-6 border rounded-lg shadow-sm">
+          {/* 결제 금액 */}
+          <div className="border p-4 rounded-lg shadow-sm bg-white">
             <div className="text-2xl my-2">
               <h2 className="font-bold">결제 금액</h2>
             </div>
-            <div>
-              <div className="flex justify-between items-center">
+            <div className="pl-4 pr-4">
+              <div className="flex justify-between items-center ">
                 <p>총 상품 가격</p>
                 <p>{totalPrice.toLocaleString()}원</p>
               </div>
               <div className="flex justify-between items-center pt-2">
-                <p>총 수수료</p>
+                <p>수수료</p>
                 <p>{totalCommission.toLocaleString()}원</p>
               </div>
               <div className="flex justify-between items-center border-b py-2">
-                <p>총 배송비</p>
+                <p>배송비</p>
                 <p>{totalShipping.toLocaleString()}원</p>
               </div>
               <div className="text-end py-2">
                 <p>{(totalPrice + totalCommission).toLocaleString()}원</p>
               </div>
             </div>
-
-            <button
-              type="submit"
-              className="w-full h-auto border rounded px-2 py-1 font-bold mt-4"
-            >
-              결제하기
-            </button>
+            <div className="flex justify-center items-center">
+              <button
+                type="submit"
+                className="w-1/2 h-auto border rounded px-2 py-1 font-bold mt-4"
+                onClick={() => handleMoveConfirmation()}
+              >
+                결제하기
+              </button>
+            </div>
           </div>
         </div>
       </div>
