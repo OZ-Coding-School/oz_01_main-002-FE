fa821d3120bef73cba77b41f020b4970e920c969
alsendrha <alsendrha@naver.com>
2024-05-29 14:51:53 +0900

    env파일 적용

--- src/api/baseApi.tsx
+++ src/api/baseApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBb6eJsDJeq2QVwlr4yu_jAQOH66_kFpXw",
-  authDomain: "team-project-b1795.firebaseapp.com",
-  projectId: "team-project-b1795",
-  storageBucket: "team-project-b1795.appspot.com",
-  messagingSenderId: "492099747016",
-  appId: "1:492099747016:web:7afa70f7dd50fe42ca39fd"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
