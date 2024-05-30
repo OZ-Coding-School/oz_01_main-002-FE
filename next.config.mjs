453e4b83d73683bb422c7e31dc63cca08145980d
alsendrha <alsendrha@naver.com>
2024-05-31 04:36:55 +0900

    옵션 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  trailingSlash: true,
   images: {
     domains: ["firebasestorage.googleapis.com"],
     domains: ["mpg-image-bucket.s3.amazonaws.com"],
