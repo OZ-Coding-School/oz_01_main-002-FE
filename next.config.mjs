3af8127d2836d266fc3cdcfe31c23e791c63cf46
alsendrha <alsendrha@naver.com>
2024-06-02 14:30:15 +0900

    설정변경

--- next.config.mjs
+++ next.config.mjs
@@ -9,24 +9,8 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
+  trailingSlash: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path((?!.*\\.html$).*)", // 모든 경로에서 .html이 없는 경우
-        destination: "/:path.html", // .html 확장자를 추가
-        permanent: true,
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/:path*.html", // .html 확장자를 포함한 경로
-        destination: "/:path*", // 실제 파일 경로에서 .html 제거
-      },
-    ];
-  },
 };
 
 export default nextConfig;
