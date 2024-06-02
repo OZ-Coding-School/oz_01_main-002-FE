30d586fd57e3141c96e0686eb7f69722dcbfd09b
alsendrha <alsendrha@naver.com>
2024-06-02 14:24:52 +0900

    설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,30 @@ const nextConfig = {
   reactStrictMode: false,
   output: "export",
   images: {
-    domains: ["firebasestorage.googleapis.com"],
-    domains: ["mpg-image-bucket.s3.amazonaws.com"],
+    domains: [
+      "firebasestorage.googleapis.com",
+      "mpg-image-bucket.s3.amazonaws.com",
+    ],
     unoptimized: true,
   },
-  // swcMinify: true,
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path((?!.*\\.html$).*)", // 모든 경로에서 .html이 없는 경우
+        destination: "/:path.html", // .html 확장자를 추가
+        permanent: true,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:path*.html", // .html 확장자를 포함한 경로
+        destination: "/:path*", // 실제 파일 경로에서 .html 제거
+      },
+    ];
+  },
 };
 
 export default nextConfig;
