2c03934d75b4f939b0bc6a9aa9584fc6e9651d57
alsendrha <alsendrha@naver.com>
2024-06-04 13:10:59 +0900

    리프레시 에러 처리

--- src/api/baseApi.tsx
+++ src/api/baseApi.tsx
@@ -7,14 +7,14 @@ const apiClient = axios.create({
 })
 
 async function postRefreshToken() {
-  const response = await apiClient.post('/api/v1/users/refresh', {
-  }, {
+  const response = await apiClient.post('/api/v1/users/refresh', {}, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('refresh_token')}`
     }
-  })
+  });
   return response;
 }
+
 apiClient.interceptors.response.use((config) => {
   const accessToken = localStorage.getItem('access_token');
   if (accessToken) {
@@ -30,35 +30,39 @@ apiClient.interceptors.response.use(
   async (error) => {
     const { config, response } = error;
     if (response) {
-      const { status } = response;
-      if (localStorage.getItem('refresh_token') && status === 401) {
-        if (!config._retry) {
-          config._retry = true;
+      const { status, config: { url } } = response;
+      console.log('error', url);
+      if (url === '/api/v1/users/refresh' && status === 401) {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+        localStorage.removeItem('user_id');
+        window.location.href = '/login';
+      } else {
+        if (localStorage.getItem('refresh_token') && status === 401) {
           try {
-            const tokenResponse = await postRefreshToken();
-            if (tokenResponse.status === 200) {
-              const newAccessToken = tokenResponse.data.access_token;
-              localStorage.setItem('access_token', newAccessToken);
-              localStorage.setItem('user_id', tokenResponse.data.user);
-              axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
-              config.headers.Authorization = `Bearer ${newAccessToken}`;
-              return axios(config);
-            } else {
-              console.error('error:', error);
-              localStorage.removeItem('access_token');
-              localStorage.removeItem('user_id');
+            if (!config._retry) {
+              config._retry = true;
+              const tokenResponse = await postRefreshToken();
+              if (tokenResponse.status === 200) {
+                const newAccessToken = tokenResponse.data.access_token;
+                localStorage.setItem('access_token', newAccessToken);
+                localStorage.setItem('user_id', tokenResponse.data.user);
+                axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+                config.headers.Authorization = `Bearer ${newAccessToken}`;
+                return axios(config);
+              } else {
+                throw new Error('토큰 갱신 실패');
+              }
             }
           } catch (refreshError) {
             console.log('토큰 갱신 실패', refreshError);
             localStorage.removeItem('access_token');
+            localStorage.removeItem('refresh_token');
             localStorage.removeItem('user_id');
+
           }
         }
       }
-    } else {
-      console.error('No response received:', error);
-      localStorage.removeItem('access_token');
-      localStorage.removeItem('user_id');
     }
     return Promise.reject(error);
   }
